3b40d7464586f215f2563e3b2f34a8d1dc75f4a4
jackthegreathacker <109450895+jackthegreathacker@users.noreply.github.com>
2022-07-17 18:58:27 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -51,8 +51,8 @@ async def cb_handler(client, query):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/star_movies_hd_telugu"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/star_movies_hd_telugu_group")
                 ]
             ]
         )
